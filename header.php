06cc40fad9bce4d277043febc13a2dc74231a9d0
Andreea Butiu <deea.b22@gmail.com>
2022-02-21 12:34:54 +0200

    Search on mobile

--- header.php
+++ header.php
@@ -5,59 +5,44 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<!-- Bootstrap core CSS -->
 		<link href="/assets/css/bootstrap.min.css" rel="stylesheet">
-		<script src="https://kit.fontawesome.com/181b72cb91.js" crossorigin="anonymous"></script>
 		<link rel="preconnect" href="https://fonts.gstatic.com">
-		<link href="https://fonts.googleapis.com/css2?family=Kumbh+Sans:wght@500&display=swap" rel="stylesheet">
+		<!--<link href="https://fonts.googleapis.com/css2?family=Kumbh+Sans:wght@300;700&display=swap" rel="stylesheet">-->
+		<link rel="stylesheet" href="https://use.typekit.net/ilo5uba.css">
+		<script src="https://kit.fontawesome.com/181b72cb91.js" crossorigin="anonymous"></script>
 		<meta name="theme-color" content="#ffc266">
 		<!-- Bootstrap core CSS -->
 		<link href="/assets/css/custom.css" rel="stylesheet">
+		<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+		<title>Shindig</title>
 	</head>
 	<body>
-		<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container">
-	  <a class="navbar-brand" href="/">
-       <img src="/assets/images/logo.svg" alt="" height="30" class="d-inline-block align-text-top">
-       <span class="d-none d-lg-inline-block">Shin Diggity Dog</span>
-     </a>
-	 <div class="ms-auto me-2">
+		<? if (!isset($nonav)) { ?>
+			<nav class="navbar navbar-expand-lg navbar-light">
+				<div class="container">
 
-		 <a href="login.php" class="btn btn-primary border-width-2 d-inline-block d-lg-none"><i class="fas fa-lock"></i></a>
-		 <a href="account.php" class="btn btn-primary border-width-2 d-inline-block d-lg-none"><i class="fas fa-user-circle"></i></a>
-		 <a href="logout.php" class="btn btn-secondary border-width-2 d-inline-block d-lg-none"><i class="fas fa-door-open"></i></a>
+					<a class="navbar-brand order-md-1" href="/"><span class="d-inline-block">SHINDIG</span></a>
 
-	 </div>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
+					<div class="ms-2 right-navigation order-md-3">
+						<a href="/" class="btn"><i class="fa-solid fa-house"></i></a>
+						<a href="#search" class="btn d-inline-block d-md-none" data-bs-toggle="collapse"><i class="fa-regular fa-magnifying-glass"></i></a>
+						<? if (!isset($_COOKIE['id']) || ($_COOKIE['id'] == NULL) || !is_numeric($_COOKIE['id'])) { ?>
+							<a href="login.php" class="btn"><i class="fa-light fa-sign-in"></i></a>
+						<? } ?>
+						<? if (isset($_COOKIE['id']) && ($_COOKIE['id'] != NULL) && is_numeric($_COOKIE['id'])) { ?>
+							<a href="" class="btn"><i class="fa-light fa-compass"></i></a>
+							<a href="favorites.php" class="btn"><i class="fa-light fa-star"></i></a>
+							<a href="event_manage.php" class="btn"><i class="fa-light fa-calendar-day"></i></a>
+							<a href="user_edit.php" class="btn"><i class="fa-light fa-user"></i></i></a>
+							<a href="logout.php" class="btn"><i class="fa-light fa-sign-out"></i></a>
+						<? } ?>
+					</div>
 
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-
-        <div class="main-navigation me-auto mb-2 mb-lg-0">
-			<ul class="navbar-nav">
-	          <li class="nav-item">
-	            <a class="nav-link active" aria-current="page" href="/">Home</a>
-	          </li>
-	          <li class="nav-item">
-	            <a class="nav-link" href="most_views.php">Popular</a>
-	          </li>
-	          <li class="nav-item">
-	            <a class="nav-link" href="favorites.php">Favorites</a>
-	          </li>
-	        </ul>
-		</div>
-		<form class="d-flex ms-auto">
-			<input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-			<button class="btn btn-outline-success" type="submit">Search</button>
-		</form>
-		<div class="ms-2">
-
-			<a href="login.php" class="btn btn-primary border-width-2 d-none d-lg-inline-block"><i class="fas fa-lock"></i><span class="ms-2 d-inline-block">Log In</span></a>
-			<a href="account.php" class="btn btn-primary border-width-2 d-none d-lg-inline-block"><i class="fas fa-user-circle"></i><span class="ms-2 d-inline-block">Account</span></a>
-			<a href="logout.php" class="btn btn-secondary border-width-2 d-none d-lg-inline-block"><i class="fas fa-door-open"></i><span class="ms-2 d-inline-block">Log Out</span></a>
-
-		</div>
-
-    </div>
-    </div>
-  </div>
-</nav>
+					<form id="search" class="d-md-flex mx-auto order-md-2 search-form-element collapse">
+						<div class="search-container">
+							<span><i class="fa-regular fa-magnifying-glass"></i></span>
+							<input class="form-control search" type="search" aria-label="Search">
+						</div>
+					</form>
+				</div>
+			</nav>
+		<? } ?>
